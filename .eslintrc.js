82cb074cf6caed3540f9a4260a5077f9be2c696c
bbennett7 <bryn.bennett@gmail.com>
2020-03-13 20:11:37 -0700

    File setup

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    extends: [
+      'airbnb-base',
+      'prettier/react',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended'
+    ],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': ['error'],
+      'class-methods-use-this': 0,
+      'no-console': 0,
+      'react/prop-types': 0,
+      'no-underscore-dangle': 1,
+      'react/no-string-refs': 1
+    },
+    settings: {
+      react: {
+        version: '16.9.0'
+      }
+    },
+    parser: 'babel-eslint',
+    env: {
+      jest: true,
+      browser: true,
+      node: true
+    }
+  };

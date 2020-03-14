12f336f5519496ea896d3051fa8e8d34cc92e123
bbennett7 <bryn.bennett@gmail.com>
2020-03-14 02:07:06 -0700

    User context

--- src/App.js
+++ src/App.js
@@ -2,10 +2,36 @@ import React, { PureComponent } from 'react';
 import './styles/global.scss';
 import firebase from './config/firebase-config';
 import DefaultLayout from './containers/DefaultLayout/DefaultLayout';
+import UserContext from './context/UserContext';
 
 class App extends PureComponent {
+  state = {
+    user: {}
+  };
+
+  componentDidMount() {}
+
+  updateUser = data => {
+    return this.setState({
+      user: {
+        ...this.state.user,
+        ...data
+      }
+    });
+  };
+
   render() {
-    return <DefaultLayout />;
+    const { user } = this.state;
+    return (
+      <UserContext.Provider
+        value={{
+          user,
+          updateUser: this.updateUser
+        }}
+      >
+        <DefaultLayout />
+      </UserContext.Provider>
+    );
   }
 }
 

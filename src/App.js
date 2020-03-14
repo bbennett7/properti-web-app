64dfc62694630a17de23679b379a3e179754e11a
bbennett7 <bryn.bennett@gmail.com>
2020-03-14 12:37:39 -0700

    Fetch user data and user tasks on firebase auth change, set to user context

--- src/App.js
+++ src/App.js
@@ -3,13 +3,42 @@ import './styles/global.scss';
 import firebase from './config/firebase-config';
 import DefaultLayout from './containers/DefaultLayout/DefaultLayout';
 import UserContext from './context/UserContext';
+import { getUserById } from './api/user';
+import { getTasksByUserId } from './api/task';
 
 class App extends PureComponent {
   state = {
-    user: {}
+    user: null,
+    tasks: []
   };
 
-  componentDidMount() {}
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(async user => {
+      console.log('Auth change', user);
+
+      if (!user) {
+        return this.setState({
+          user: null,
+          tasks: []
+        });
+      }
+
+      try {
+        const userData = await getUserById(user.uid);
+        const tasksData = await getTasksByUserId(user.uid);
+
+        return this.setState({
+          user: userData.data,
+          tasks: tasksData.data
+        });
+      } catch (err) {
+        return this.setState({
+          user: null,
+          tasks: []
+        });
+      }
+    });
+  };
 
   updateUser = data => {
     return this.setState({
@@ -20,13 +49,21 @@ class App extends PureComponent {
     });
   };
 
+  updateTasks = data => {
+    return this.setState({
+      tasks: data
+    });
+  };
+
   render() {
-    const { user } = this.state;
+    const { user, tasks } = this.state;
     return (
       <UserContext.Provider
         value={{
           user,
-          updateUser: this.updateUser
+          tasks,
+          updateUser: this.updateUser,
+          updateTasks: this.updateTasks
         }}
       >
         <DefaultLayout />

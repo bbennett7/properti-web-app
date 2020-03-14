c8868e460e0d80359e3ac5f00aaaedf3345b39ec
bbennett7 <bryn.bennett@gmail.com>
2020-03-13 22:15:32 -0700

    Project structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
+import './styles/global.scss';
+import firebase from './config/firebase-config';
+import DefaultLayout from './containers/DefaultLayout/DefaultLayout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+  render() {
+    return <DefaultLayout />;
+  }
 }
 
 export default App;

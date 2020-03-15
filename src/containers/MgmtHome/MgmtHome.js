ebce108ddc0dcac7d91a637957efef9cec74aa59
bbennett7 <bryn.bennett@gmail.com>
2020-03-14 23:02:31 -0700

    Toggle task details and edit

--- src/containers/MgmtHome/MgmtHome.js
+++ src/containers/MgmtHome/MgmtHome.js
@@ -1,7 +1,5 @@
 import React, { PureComponent } from 'react';
 import styles from './MgmtHome.module.scss';
-import firebase from '../../config/firebase-config';
-import Button from '../../components/Button/Button';
 import { getPropertiesByManagerId } from '../../api/property';
 import { getOpenTasksByManagerId } from '../../api/task';
 import { fetchYelpServices } from '../../api/yelp';
@@ -9,9 +7,11 @@ import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 // import helpers from '../../helpers';
 import UserContext from '../../context/UserContext';
 import { ReactComponent as Filter } from '../../assets/filter.svg';
+import { ReactComponent as Expand } from '../../assets/add.svg';
 
 class MgmtHome extends PureComponent {
   state = {
+    user: {},
     properties: [],
     openTasks: [],
     loading: true,
@@ -20,24 +20,38 @@ class MgmtHome extends PureComponent {
     error: '',
     searchMessage: '',
     activeFilter: false,
+    activeUpdate: false,
+    activeUpdateId: '',
     filteredTasks: [],
-    filterByProperties: []
+    filterByProperties: [],
+    expandedTask: ''
   };
 
   componentDidMount() {
     this.fetchData();
   }
 
+  componentDidUpdate() {
+    if (Object.keys(this.state.user).length === 0) {
+      this.fetchData();
+    }
+  }
+
   fetchData = async () => {
     const { user } = this.context;
 
-    const propertiesData = await getPropertiesByManagerId(user.id);
-    const tasksData = await getOpenTasksByManagerId(user.id);
+    if (!user) {
+      return null;
+    }
 
-    this.setState({
+    const propertiesData = await getPropertiesByManagerId(user.id);
+    const openTasksData = await getOpenTasksByManagerId(user.id);
+
+    return this.setState({
       properties: propertiesData.data,
-      openTasks: tasksData.data,
-      loading: false
+      openTasks: openTasksData.data,
+      loading: false,
+      user
     });
   };
 
@@ -60,7 +74,7 @@ class MgmtHome extends PureComponent {
       if (data.data.length === 0) {
         return this.setState({
           loadingServices: false,
-          searchMessage: 'No services found for this building\'s area.'
+          searchMessage: "No services found for this building's area."
         });
       }
 
@@ -75,9 +89,41 @@ class MgmtHome extends PureComponent {
     }
   };
 
+  toggleUpdate = event => {
+    event.preventDefault();
+    return this.setState({
+      activeUpdate: !this.state.activeUpdate,
+      activeUpdateId: this.state.activeUpdate ? '' : event.currentTarget.id
+    });
+  };
+
+  updateTaskStatus = async event => {
+    event.preventDefault();
+    console.log('clicked');
+  };
+
+  toggleExpanded = event => {
+    event.preventDefault();
+    const { expandedTask } = this.state;
+    const { id } = event.currentTarget;
+
+    return this.setState({
+      expandedTask: expandedTask === id ? '' : id,
+      activeUpdate: false,
+      activeUpdateId: ''
+    });
+  };
+
   renderOpenTasks = () => {
-    const { openTasks, filteredTasks, filterByProperties } = this.state;
-    const tasks = filterByProperties.length > 0 ? filteredTasks : openTasks;
+    const {
+      openTasks,
+      filteredTasks,
+      filterByProperties,
+      activeUpdateId,
+      expandedTask,
+      activeFilter
+    } = this.state;
+    const tasks = filterByProperties.length > 0 && activeFilter ? filteredTasks : openTasks;
     return tasks.map(t => {
       const { task, resident, property } = t;
 
@@ -85,25 +131,51 @@ class MgmtHome extends PureComponent {
         <div className={styles.taskWrapper} key={t.id}>
           <div className={styles.taskTitle}>
             {property.name} - Unit {resident.unit} - {task.name} - {t.urgency_level} Priority
+            <Expand className={styles.icon} id={t.id} onClick={this.toggleExpanded} />
           </div>
-          <div className={styles.taskDetails}>
-            <div className={styles.taskData}>
-              <div className={styles.dataLabel}>Resident name:</div>
-              {resident.first_name} {resident.last_name}
-            </div>
-            <div className={styles.taskData}>
-              <div className={styles.dataLabel}>Notes from the resident:</div>
-              {t.notes}
-            </div>
-            <div className={styles.actionsWrapper}>
-              <a href={`mailto:${resident.email}`} className={styles.taskData}>
-                Contact Resident
-              </a>
-              <div className={styles.taskData} onClick={this.getServices} id={t.id}>
-                Find a service
+          {expandedTask !== t.id ? null : (
+            <div className={styles.taskDetails}>
+              <div className={styles.taskData}>
+                <div className={styles.dataLabel}>Resident name:</div>
+                {resident.first_name} {resident.last_name}
+              </div>
+              <div className={styles.taskData}>
+                <div className={styles.dataLabel}>Notes from the resident:</div>
+                {t.notes}
+              </div>
+              <div className={styles.taskData}>
+                <div className={styles.dataLabel} id={t.id}>
+                  Status:
+                </div>
+                {t.status}
+              </div>
+              <div className={styles.actionsWrapper}>
+                <a href={`mailto:${resident.email}`} className={styles.action}>
+                  Contact Resident
+                </a>
+                <div className={styles.action} onClick={this.toggleUpdate} id={t.id}>
+                  Update Status
+                </div>
+                <div className={styles.action} onClick={this.getServices} id={t.id}>
+                  Find a service
+                </div>
               </div>
             </div>
-          </div>
+          )}
+
+          {activeUpdateId !== t.id ? null : (
+            <div className={styles.updateWrapper}>
+              Update Status for {property.name}, Unit {resident.unit}
+              <form onSubmit={this.updateTaskStatus}>
+                <select id={t.id} name={t.name}>
+                  <option value="Open">Open</option>
+                  <option value="In Progress">In Progress</option>
+                  <option value="Complete">Complete</option>
+                </select>
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+          )}
         </div>
       );
     });
@@ -115,7 +187,7 @@ class MgmtHome extends PureComponent {
       return (
         <div className={styles.serviceWrapper} key={s.id}>
           <div className={styles.serviceTitle}>
-            <a href={s.url} target="_blank">
+            <a href={s.url} target="_blank" rel="noopener noreferrer">
               {s.name}
             </a>
             &nbsp;{s.rating} stars ({s.review_count} reviews) - ${s.price}
@@ -132,7 +204,9 @@ class MgmtHome extends PureComponent {
 
   toggleFilter = () => {
     return this.setState({
-      activeFilter: !this.state.activeFilter
+      activeFilter: !this.state.activeFilter,
+      filteredTasks: this.state.activeFilter ? [] : this.state.filteredTasks,
+      filterByProperties: this.state.activeFilter ? [] : this.state.filterByProperties
     });
   };
 
@@ -181,7 +255,6 @@ class MgmtHome extends PureComponent {
   };
 
   render() {
-    const { user } = this.context;
     const { loading, loadingServices, localServices, activeFilter, searchMessage } = this.state;
 
     if (loading) {

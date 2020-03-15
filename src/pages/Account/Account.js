37777b12f0c0d0441242310cef008d3e1dc0ed20
bbennett7 <bryn.bennett@gmail.com>
2020-03-15 01:26:48 -0700

    User can update building

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -4,9 +4,35 @@ import firebase from '../../config/firebase-config';
 import Button from '../../components/Button/Button';
 // import helpers from '../../helpers';
 import UserContext from '../../context/UserContext';
+import { getProperties } from '../../api/property';
+import { updateUser } from '../../api/user';
 
 class Account extends PureComponent {
-  state = {};
+  state = {
+    activeUpdate: false,
+    properties: [],
+    updatedProperty: {
+      id: '',
+      unit: '',
+      error: ''
+    }
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    try {
+      const data = await getProperties();
+
+      return this.setState({
+        properties: data.data
+      });
+    } catch (err) {
+      return err;
+    }
+  };
 
   signOut = async event => {
     event.preventDefault();
@@ -25,9 +51,78 @@ class Account extends PureComponent {
       });
   };
 
+  renderUpdateForm = () => {
+    const { properties } = this.state;
+    return (
+      <form onSubmit={this.handleOnSubmit} className={styles.form}>
+        <select onChange={this.handleOnChange} id={'id'}>
+          <option value="Select">Select Building</option>
+          {properties.map(p => (
+            <option value={p.id} key={p.id}>
+              {p.name}
+            </option>
+          ))}
+        </select>
+        <div>
+          <label>Unit</label>
+          <input id="unit" onChange={this.handleOnChange} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  };
+
+  toggleUpdate = () => {
+    this.setState({
+      activeUpdate: !this.state.activeUpdate
+    });
+  };
+
+  handleOnChange = event => {
+    event.preventDefault();
+    if (event.target.value === 'Select') {
+      return this.setState({
+        updatedProperty: {
+          ...this.state.updatedProperty,
+          id: ''
+        }
+      });
+    }
+
+    const update = {};
+    update[event.target.id] = event.target.value;
+
+    return this.setState({
+      updatedProperty: {
+        ...this.state.updatedProperty,
+        ...update
+      }
+    });
+  };
+
+  handleOnSubmit = async () => {
+    const { id } = this.context.user;
+    const { updatedProperty } = this.state;
+    const body = {
+      property_id: updatedProperty.id,
+      unit: updatedProperty.unit
+    };
+    console.log(id, updatedProperty);
+    try {
+      const data = await updateUser(id, body);
+      console.log(data.data);
+      this.context.updateUser(data.data);
+      console.log(this.context);
+    } catch (err) {
+      this.setState({
+        updatedProperty: err
+      });
+    }
+  };
+
   render() {
     const { user } = this.context;
-
+    const { activeUpdate } = this.state;
     if (!user) {
       return null;
     }
@@ -37,11 +132,10 @@ class Account extends PureComponent {
         <div className={styles.header}>Account</div>
         <div className={styles.accountInfoWrapper}>
           <div className={styles.col}>
-            <div className={styles.fieldTitle}>First Name</div>
-            <div className={styles.fieldData}>{user.first_name}</div>
-
-            <div className={styles.fieldTitle}>Last Name</div>
-            <div className={styles.fieldData}>{user.last_name}</div>
+            <div className={styles.fieldTitle}>Name</div>
+            <div className={styles.fieldData}>
+              {user.first_name}&nbsp;{user.last_name}
+            </div>
 
             <div className={styles.fieldTitle}>Email</div>
             <div className={styles.fieldData}>{user.email}</div>
@@ -49,19 +143,34 @@ class Account extends PureComponent {
           <div className={styles.col}>
             {!user.property ? (
               <div className={styles.fieldData}>
-                You have not added your building to your account yet. Please select from the list
-                below, and add your unit number.
+                You have not added your building to your account yet. Please select your building
+                from the list below and add your unit number.
+                {this.renderUpdateForm()}
               </div>
             ) : (
               <React.Fragment>
-                <div className={styles.fieldTitle}>Property Name</div>
-                <div className={styles.fieldData}>{user.first_name}</div>
+                <div className={styles.fieldTitle}>Property</div>
+                <div className={styles.fieldData}>
+                  {user.property.name}, Unit &nbsp;{user.unit}
+                </div>
 
-                <div className={styles.fieldTitle}>Property Manager</div>
-                <div className={styles.fieldData}>{user.last_name}</div>
-
-                <div className={styles.fieldTitle}>Email</div>
-                <div className={styles.fieldData}>{user.email}</div>
+                {user.account_type === 'Building Manager' ? null : (
+                  <React.Fragment>
+                    <div className={styles.fieldTitle}>Property Manager</div>
+                    <div className={styles.fieldData}>
+                      {user.property_manager.first_name}&nbsp;{user.property_manager.last_name}
+                    </div>
+                    <div className={styles.fieldData}>
+                      <a href={`mailto:${user.property_manager.email}`}>
+                        Contact your property manager
+                      </a>
+                    </div>
+                  </React.Fragment>
+                )}
+                <div className={styles.movedText}>
+                  Moved recently?&nbsp;<div onClick={this.toggleUpdate}>Update your building.</div>
+                </div>
+                {activeUpdate ? this.renderUpdateForm() : null}
               </React.Fragment>
             )}
           </div>

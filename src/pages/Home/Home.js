30f109e6e75c4601728782585f0919092dc65302
bbennett7 <bryn.bennett@gmail.com>
2020-03-15 14:09:53 -0700

    Conditionally render reshome container

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import styles from './Home.module.scss';
 import UserContext from '../../context/UserContext';
 import MgmtHome from '../../containers/MgmtHome/MgmtHome';
+import ResHome from '../../containers/ResHome/ResHome';
 
 const Home = () => {
   const userContext = useContext(UserContext);
@@ -9,7 +10,7 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>Home</div>
-      {userContext.user.account_type === 'Building Manager' ? <MgmtHome /> : null}
+      {userContext.user.account_type === 'Building Manager' ? <MgmtHome /> : <ResHome />}
     </div>
   );
 };
